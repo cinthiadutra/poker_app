7132524dcc667be4d46223a2ce4e71cd2e727da9
Cinthia Dutra <cinthia.dutra@datacoress>
2025-03-24 10:43:52 -0300

    acertos firebase

--- poker_app/lib/main.dart
+++ poker_app/lib/main.dart
@@ -2,33 +2,33 @@ import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:poker_app/app_module.dart';
 import 'package:poker_app/core/utils/app_colors.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_analytics/observer.dart';
+import 'firebase_options.dart';
 
 void main() async {
-  runApp(ModularApp(module: AppModule(), child: const AppWidget()) );
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
+  runApp(ModularApp(module: AppModule(), child:  AppWidget()));
 }
 
 class AppWidget extends StatelessWidget {
-  const AppWidget({super.key});
+  final FirebaseAnalytics analytics = FirebaseAnalytics.instance;
+  AppWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
       title: 'pokerAPP',
-   
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: AppColors.secundary, primary: AppColors.primary),
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: AppColors.secundary, primary: AppColors.primary),
         useMaterial3: true,
-      ),routerConfig: Modular.routerConfig,
+      ),
+      routerConfig: Modular.routerConfig,
     );
   }
 }
-
-
-  
-  
-  
-
-  
-  
-  
-  
